bebc2c005ba46eeee860e3e16613fd71a677876f
kangjietan <kangjietan@gmail.com>
2021-07-12 15:10:37 -0700

    Connect to local psql.

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,9 @@
-const Sequelize = require("sequelize");
+require("dotenv").config();
+const { Sequelize } = require('sequelize');
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
+const db = new Sequelize("messenger", process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: "localhost",
+  dialect: "postgres",
 });
 
 module.exports = db;

17516958fe59f6237927fa0caaa9582a5e2185ee
kangjietan <kangjietan@gmail.com>
2021-07-12 16:18:33 -0700

    Refactor to sort by message creation time rather than reversing array.

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -3,12 +3,16 @@ import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
 
+const sortByCreatedDate = (a, b) => {
+  return new Date(a.createdAt) - new Date(b.createdAt);
+}
+
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
 
   return (
     <Box>
-      {messages.slice().reverse().map((message) => {
+      {messages.slice().sort(sortByCreatedDate).map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
         return message.senderId === userId ? (

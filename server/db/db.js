06740e39a117a338c903516c56f3b1d357587e56
kangjietan <kangjietan@gmail.com>
2021-07-14 21:27:25 -0700

    Change to environment variable

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require('sequelize');
 
 const db = new Sequelize("messenger", process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  host: "localhost",
+  host: process.env.DB_URL,
   dialect: "postgres",
 });
 

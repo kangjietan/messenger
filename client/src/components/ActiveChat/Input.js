249d64502f0e622ae3b523e387d27035cf33b034
kangjietan <kangjietan@gmail.com>
2021-07-15 22:39:23 -0700

    Ticket 4: Refactor Input and Chat components to function components

--- client/src/components/ActiveChat/Input.js
+++ client/src/components/ActiveChat/Input.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { FormControl, FilledInput } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 import { connect } from "react-redux";
@@ -17,55 +17,44 @@ const styles = {
   },
 };
 
-class Input extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: "",
-    };
-  }
+const Input = (props) => {
+  const { classes, otherUser, conversationId, user, postMessage } = props;
+  const [messageText, setMessageText] = useState("");
 
-  handleChange = (event) => {
-    this.setState({
-      text: event.target.value,
-    });
+  const handleChange = (event) => {
+    setMessageText(event.target.value);
   };
 
-  handleSubmit = async (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     // add sender user info if posting to a brand new convo, so that the other user will have access to username, profile pic, etc.
     const reqBody = {
       text: event.target.text.value,
-      recipientId: this.props.otherUser.id,
+      recipientId: otherUser.id,
       read: false,
-      conversationId: this.props.conversationId,
-      sender: this.props.conversationId ? null : this.props.user,
+      conversationId: conversationId,
+      sender: conversationId ? null : user,
     };
 
-    await this.props.postMessage(reqBody);
-    this.setState({
-      text: "",
-    });
+    await postMessage(reqBody);
+    setMessageText("");
   };
 
-  render() {
-    const { classes } = this.props;
-    return (
-      <form className={classes.root} onSubmit={this.handleSubmit}>
-        <FormControl fullWidth hiddenLabel>
-          <FilledInput
-            classes={{ root: classes.input }}
-            disableUnderline
-            placeholder="Type something..."
-            value={this.state.text}
-            name="text"
-            onChange={this.handleChange}
-          />
-        </FormControl>
-      </form>
-    );
-  }
-}
+  return (
+    <form className={classes.root} onSubmit={handleSubmit}>
+      <FormControl fullWidth hiddenLabel>
+        <FilledInput
+          classes={{ root: classes.input }}
+          disableUnderline
+          placeholder="Type something..."
+          value={messageText}
+          name="text"
+          onChange={handleChange}
+        />
+      </FormControl>
+    </form>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
